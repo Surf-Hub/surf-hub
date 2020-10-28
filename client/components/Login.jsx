bce3162419507170a2440809237b50f8a1eb65fc
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-10-27 19:55:48 -0700

    created navToHome function for button press. will change to OAuth later.
    Co-authored-by: Liam <liameno16@gmail.com>
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Jason <jason.victor26@gmail.com>

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -6,16 +6,19 @@ import styles from '../styles';
 
 const logo = require('../../assets/logo.jpg');
 
-export default function Homescreen() {
+export default function Login({ navigation }) {
   // const handlePress = () => {
   //   Linking.openURL('http://localhost:3000/auth/linkedin');
   // }
+  const navToHome = () => {
+    navigation.navigate('Home Page');
+  };
 
   return (
     <View style={styles.container}>
       <Image source={logo} style={{ width: 75, height: 75 }} />
       <Text style={styles.loginText}>Login</Text>
-      <Button style={styles.loginButton} title="Sign in with Google" />
+      <Button style={styles.loginButton} title="Sign in with Google" onPress={navToHome} />
     </View>
   );
 }
