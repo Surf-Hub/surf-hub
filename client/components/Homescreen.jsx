ad50ee6dd274b7ec56b3ec8fc19272ccc253c8b6
Alexander Kim <akim3235@gmail.com>
2020-10-27 17:02:27 -0700

    Set up the project
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Jason <jason.victor26@gmail.com>
    Co-authored-by: Alex <Alexander.Landeros1@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

---
+++ client/components/Homescreen.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text, View, Image, Button } from 'react-native';
+import * as Linking from 'expo-linking';
+import styles from '../styles';
+
+const logo = require('../../assets/logo.jpg');
+
+export default function Homescreen() {
+  const handlePress = () => {
+    Linking.openURL('http://localhost:3000/auth/linkedin');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={{width: 75, height: 75}}/>
+      <Text style={styles.loginText}>Login</Text>
+      <Button style={styles.loginButton} title="Sign in with LinkedIn" onPress={handlePress} />
+    </View>
+  );
+}
---
+++ client/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 'large',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
+  loginButton: {
+    marginBottom: '1rem',
+    padding: '0.5rem',
+  },
+});
+
+export default styles;
