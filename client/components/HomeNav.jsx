5924655289c53ba4f9ef8afb24aed3318ebbed0f
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-10-27 20:38:08 -0700

    added comments to homeNav component
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Jason <jason.victor26@gmail.com>
    Co-authored-by: Liam <liameno16@gmail.com>

--- client/components/HomeNav.jsx
+++ client/components/HomeNav.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Text, View, Image, Button } from 'react-native';
 import styles from '../styles';
 
+// imported our logo and generic profile pic for now.
 const logo = require('../../assets/logo.jpg');
 const liamProfilePic = require('../../assets/Surfer_Liam.png');
 
+// created the navBar component with logo, user image and app name.
 export default function HomeNav() {
   return (
     <View style={styles.homeNavBar}>
