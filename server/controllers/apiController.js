564f7c93b1c7b80a8fd254fb2f89746d32719b4b
Jay <36286598+Theqwertypusher@users.noreply.github.com>
2020-10-29 00:47:58 -0400

    (created) controllers folder

---
+++ server/controllers/apiController.js
@@ -0,0 +1,74 @@
+const apiController = {};
+const fetch = require('node-fetch');
+
+apiController.getWeatherData = async (req, res, next) => {
+  const { lat, lng } = req.body;
+
+  let start = new Date().toISOString().replace(':', '%3A').replace(':', '%3A').split('.');
+  start[1] = '%2B00%3A00';
+  start = start.join('');
+  res.locals.start = start;
+
+  let end = new Date();
+  end.setMinutes(end.getMinutes() + 1);
+  end = end.toISOString().replace(':', '%3A').replace(':', '%3A').split('.');
+  end[1] = '%2B00%3A00';
+  end = end.join('');
+  res.locals.end = end;
+
+  const weatherParams = 'swellHeight,waterTemperature,windDirection';
+  const apiKey = '945e95b2-19a0-11eb-b3db-0242ac130002-945e9698-19a0-11eb-b3db-0242ac130002';
+  const apiUrl = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${weatherParams}&start=${start}&end=${end}`;
+  const configObj = {
+    headers: {
+      Authorization: apiKey,
+    },
+  };
+
+  try {
+    await fetch(apiUrl, configObj)
+      .then((res) => res.json())
+      .then((data) => {
+        const getAvg = (dataObj) => {
+          let values = Object.values(dataObj);
+          let avg = values.reduce((acc, cur) => acc + cur) / values.length;
+          return avg.toFixed(2);
+        };
+
+        const surfConditions = {
+          swellHeight: getAvg(data.hours[0].swellHeight) * 3.28,
+          waterTemperature: (getAvg(data.hours[0].waterTemperature)*9/5) + 32,
+          windDirection: getAvg(data.hours[0].windDirection) * 1,
+        };
+        res.locals.surfConditions = surfConditions;
+        return next();
+      });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+apiController.getTideData = async (req, res, next) => {
+  const { lat, lng } = req.body;
+
+  const apiKey = '945e95b2-19a0-11eb-b3db-0242ac130002-945e9698-19a0-11eb-b3db-0242ac130002';
+  const apiUrl = `https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=${res.locals.start}&end=${res.locals.end}`;
+  const configObj = {
+    headers: {
+      Authorization: apiKey,
+    },
+  };
+
+  try {
+    await fetch(apiUrl, configObj)
+      .then((res) => res.json())
+      .then((data) => {
+        res.locals.surfConditions.tide = data.data[0].type;
+        return next();
+      });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = apiController;
