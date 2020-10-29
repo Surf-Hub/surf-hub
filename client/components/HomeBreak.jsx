ce467b34f4070d6b009a675de2b3b0847c6ab58e
liameno16 <liameno16@gmail.com>
2020-10-28 21:07:53 -0700

    added hooks to trigger state change in home break on form submission
    Co-authored-by: Alex <akim3235@gmail.com>
    Co-authored-by: Stefan <Stefan.pougatchev@gmail.com>
    Co-authored-by: Jason <jason.victor26@gmail.com>
    Co-authored-by: Alex <Alexander.Landeros1@gmail.com>

--- client/components/HomeBreak.jsx
+++ client/components/HomeBreak.jsx
@@ -1,30 +1,36 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { View, Text, TouchableHighlight, Modal, Button } from 'react-native';
 import ViewMoreModal from './ViewMoreModal';
 import styles from '../styles';
 
-export default function HomeBreak() {
+export default function HomeBreak(props) {
   // State that toggles whether modal is visible or not
   const [modalVisible, setModalVisible] = useState(false);
-
-  const dummyData = {
-    location: 'Manhattan Beach',
-    swellHeight: '1-2 ft',
-    h2oTemp: '63-66 F',
-    weather: '67 F',
-    windDirection: '2 kts 119 SW',
-    highLowTide: 'low 3.9 ft',
-  };
+  // Home break data from props
+  const { homeBreak } = props;
+  // const dummyData = {
+  //   location: 'Manhattan Beach',
+  //   latitude: 46.9718,
+  //   longitude: 73.2174,
+  //   swellHeight: '1-2 ft',
+  //   h2oTemp: '63-66 F',
+  //   weather: '67 F',
+  //   windDirection: '2 kts 119 SW',
+  //   highLowTide: 'low 3.9 ft',
+  // };
 
   return (
     <View style={styles.homeBreakBox}>
       {/* Weather Data for home break spot */}
-      <Text>Location: {dummyData.location}</Text>
-      <Text>Swell Height: {dummyData.swellHeight}</Text>
-      <Text>H2O Temp: {dummyData.h2oTemp}</Text>
-      <Text>Weather: {dummyData.weather}</Text>
-      <Text>Wind Direction: {dummyData.windDirection}</Text>
-      <Text>High/Low Tide: {dummyData.highLowTide}</Text>
+      <Text>Name: {homeBreak.name}</Text>
+      <Text>Latitude: {homeBreak.latitude}</Text>
+      <Text>Longitude: {homeBreak.longitude}</Text>
+      <Text>Swell Height: {homeBreak.swellHeight}</Text>
+      <Text>H2O Temp: {homeBreak.h2oTemp}</Text>
+      <Text>Weather: {homeBreak.weather}</Text>
+      <Text>Wind Direction: {homeBreak.windDirection}</Text>
+      <Text>High/Low Tide: {homeBreak.highLowTide}</Text>
 
       {/* Button to trigger modal popup */}
       <TouchableHighlight style={styles.viewMore} onPress={() => setModalVisible(true)}>
--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,9 +1,9 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { Text, View, Image, Button, TextInput } from 'react-native';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { Text, View, Image, Button, TextInput, Pressable } from 'react-native';
 import styles from '../styles';
 
-export default function SearchBar() {
+export default function SearchBar(props) {
   // Default search is empty strings for all input boxes
   const defaultSearch = {
     name: '',
@@ -12,6 +12,23 @@ export default function SearchBar() {
   };
   // searchBar is the value of the text input in the search bar
   const [searchBar, setSearchBar] = useState(defaultSearch);
+  // Function that gets invoked after submitting search bar
+  const submitSearch = () => {
+    // Fetch data about home break from backend
+    // Set home break to the response object
+    props.setHomeBreak({
+      name: searchBar.name,
+      latitude: searchBar.latitude,
+      longitude: searchBar.longitude,
+      swellHeight: '',
+      h2oTemp: '',
+      weather: '',
+      windDirection: '',
+      highLowTide: '',
+    });
+    // Reset inputs to empty strings
+    setSearchBar(defaultSearch);
+  };
 
   return (
     <View style={styles.searchBar}>
@@ -36,9 +53,9 @@ export default function SearchBar() {
         value={searchBar.longitude}
         onChange={(e) => setSearchBar({ ...searchBar, longitude: e.target.value })}
       />
-      <TouchableHighlight style={styles.searchButton} onPress={() => setSearchBar(defaultSearch)}>
-        <Text style={{ color: 'white' }}>Surf's Up Bra</Text>
-      </TouchableHighlight>
+      <Pressable style={styles.searchButton} onPressIn={submitSearch}>
+        <Text style={{ color: 'white' }}>Surfs Up Bra</Text>
+      </Pressable>
     </View>
   );
 }
