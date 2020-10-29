9585e20f82ef906d1d6cab2919d58ee56e39c5f0
Jay <36286598+Theqwertypusher@users.noreply.github.com>
2020-10-29 00:48:44 -0400

    (updated/verified) routes for API requests for weather and tide data

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const apiRouter = express.Router()
+const fetch = require('node-fetch');
+const apiRouter = express.Router();
+const apiController = require('../controllers/apiController.js');
 
-apiRouter.get('/', )
+apiRouter.get('/', 
+    apiController.getWeatherData,
+    apiController.getTideData, 
+    (req, res) => {
+        res.status(200).json(res.locals.surfConditions)
+});
 
 module.exports = apiRouter;
